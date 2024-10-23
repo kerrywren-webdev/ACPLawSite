2fbdf579762f2314b31b4927ccc924e197994cc3
kerrywren-webdev <kerry@wren-media.com>
2024-10-23 16:51:37 -0400

    Initial commit with site files

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,16 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "ACP Law",
+    "email": "apelligra@acp-law.com",
+    "phoneForTel": "732-288-6162",
+    "phoneFormatted": "(732) 288-6162",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "1913 Atlantic Avenue, Suite 150",
+        "city": "Manasquan",
+        "state": "NJ",
+        "zip": "08736",
         "country": "US",
         "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.acp-law.com"
 };
